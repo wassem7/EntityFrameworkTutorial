ef34c2c345f0d74e16fa433625eca0233d224e3e
wassem7 <waseemdarkwa@gmail.com>
2023-11-28 16:03:30 +0000

    Add project files.

---
+++ Models/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Enity_Models.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
