2e5692d07094e5c05ce32d8b202eb5fed00a5f2a
wassem7 <waseemdarkwa@gmail.com>
2023-11-28 17:12:08 +0000

    Db Context

---
+++ DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity_Models.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Entity_DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ Models/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Models.Models
+{
+    public class Book
+    {
+        public int IDBook { get; set; }
+
+        public string Title { get; set; }
+
+        public string ISBN { get; set; }
+
+        public double Price { get; set; }
+    }
+}
