d07cf18405d88ab6b5f3e6ee072ed28446a366c0
wassem7 <waseemdarkwa@gmail.com>
2023-11-28 17:22:44 +0000

    DB Connection String

--- DataAccess/Data/ApplicationDbContext.cs
+++ DataAccess/Data/ApplicationDbContext.cs
@@ -1,15 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Entity_Models.Models;
+﻿using Entity_Models.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 namespace Entity_DataAccess.Data
 {
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(
+                "Server=localhost;Database=EntityTutorial;TrustServerCertificate=True;Trusted_Connection=True;MultipleActiveResultSets=true"
+            );
+        }
     }
 }
