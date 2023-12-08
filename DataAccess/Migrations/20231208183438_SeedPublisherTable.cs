5ab53b9fea9b1ee7e84f8bc1ca82c1ea1da9e5fe
wassem7 <waseemdarkwa@gmail.com>
2023-12-08 18:52:54 +0000

    Author model

---
+++ DataAccess/Migrations/20231208183438_SeedPublisherTable.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EntityDataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedPublisherTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PublisherId",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "bookId",
+                keyValue: 1,
+                column: "PublisherId",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "bookId",
+                keyValue: 2,
+                column: "PublisherId",
+                value: 3);
+
+            migrationBuilder.InsertData(
+                table: "Publishers",
+                columns: new[] { "PublisherId", "Location", "Name" },
+                values: new object[,]
+                {
+                    { 1, "North America", "Cengage" },
+                    { 2, "Canada", "Darkwa Publication" },
+                    { 3, "Dubai", "Seemzy Publication" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_PublisherId",
+                table: "Books",
+                column: "PublisherId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Publishers_PublisherId",
+                table: "Books",
+                column: "PublisherId",
+                principalTable: "Publishers",
+                principalColumn: "PublisherId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Publishers_PublisherId",
+                table: "Books");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_PublisherId",
+                table: "Books");
+
+            migrationBuilder.DeleteData(
+                table: "Publishers",
+                keyColumn: "PublisherId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Publishers",
+                keyColumn: "PublisherId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Publishers",
+                keyColumn: "PublisherId",
+                keyValue: 3);
+
+            migrationBuilder.DropColumn(
+                name: "PublisherId",
+                table: "Books");
+        }
+    }
+}
---
+++ Models/Models/Author.cs
@@ -0,0 +1,8 @@
+﻿namespace Entity_Models.Models;
+
+public class Author
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+}
