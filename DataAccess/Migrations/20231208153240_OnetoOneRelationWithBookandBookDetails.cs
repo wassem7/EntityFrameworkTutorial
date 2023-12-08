1bd708065fa2bcf789417cbe4907c7c519604e7c
wassem7 <waseemdarkwa@gmail.com>
2023-12-08 15:55:48 +0000

    One to One Relation

---
+++ DataAccess/Migrations/20231208153240_OnetoOneRelationWithBookandBookDetails.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EntityDataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class OnetoOneRelationWithBookandBookDetails : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BookId",
+                table: "BookDetails",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookDetails_BookId",
+                table: "BookDetails",
+                column: "BookId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookDetails_Books_BookId",
+                table: "BookDetails",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "bookId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookDetails_Books_BookId",
+                table: "BookDetails");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BookDetails_BookId",
+                table: "BookDetails");
+
+            migrationBuilder.DropColumn(
+                name: "BookId",
+                table: "BookDetails");
+        }
+    }
+}
